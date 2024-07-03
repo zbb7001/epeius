70aefe41fac6647606a0b7d96df80b0fcf6b87c2
zbb <101622397+zbb7001@users.noreply.github.com>
2024-07-03 09:12:28 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,7 +1,7 @@
 // worker.src.js
 import { connect } from "cloudflare:sockets";
 let password = 'auto';
-let proxyIP = 'cdn-all.xijingping.link';
+let proxyIP = '';
 // The user name and password do not contain special characters
 // Setting the address will ignore proxyIP
 // Example:  user:pass@host:port  or  host:port
